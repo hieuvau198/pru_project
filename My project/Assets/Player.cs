89937fc4929ab80a147f6491d8155423fb5f2041
Dixeliare <daoxuanlong492004@gmail.com>
2025-03-02 20:17:44 +0700

    done folder 4 video 8

--- My project/Assets/Player.cs
+++ My project/Assets/Player.cs
@@ -4,7 +4,20 @@ public class Player : MonoBehaviour
 {
     [Header("Move Info")]
     public float moveSpeed = 12f;
-    
+    public float jumpForce;
+    public float dashSpeed;
+    public float dashDuration;
+
+    [Header("Collision info")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private float wallCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+
+    public int facingDir { get; set; } = 1;
+    private bool facingRight = true;
+
     #region Components
     public Animator anim {  get; private set; }
     public Rigidbody2D rb { get; private set; }
@@ -16,6 +29,9 @@ public class Player : MonoBehaviour
 
     public PlayerIdleState idleState { get; private set; }
     public PlayerMoveState moveState { get; private set; }
+    public PlayerJumpState jumpState { get; private set; }
+    public PlayerAirState airState { get; private set; }
+    public PlayerDashState dashState { get; private set; }
     #endregion
 
 
@@ -25,7 +41,9 @@ public class Player : MonoBehaviour
 
         idleState = new PlayerIdleState(this, stateMachine, "Idle");
         moveState = new PlayerMoveState(this, stateMachine, "Move");
-
+        jumpState = new PlayerJumpState(this, stateMachine, "Jump");
+        airState  = new PlayerAirState(this, stateMachine, "Jump");
+        dashState = new PlayerDashState(this, stateMachine, "Dash");
     }
 
     private void Start()
@@ -39,10 +57,35 @@ public class Player : MonoBehaviour
     private void Update()
     {
         stateMachine.currentState.Update();
+
     }
 
-    public void SetVelocity(float xVelocity, float yVelocity)
+    public void SetVelocity(float _xVelocity, float _yVelocity)
     {
-        rb.linearVelocity = new Vector2 (xVelocity, yVelocity);
+        rb.linearVelocity = new Vector2 (_xVelocity, _yVelocity);
+        FlipController(_xVelocity);
+    }
+
+    public bool IsGroundDetected() => Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
+    }
+
+    public void Flip()
+    { 
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    public void FlipController(float _x)
+    {
+        if (_x > 0 && !facingRight)
+            Flip();
+        else if (_x < 0 && facingRight)
+            Flip();
     }
 }
--- My project/Assets/PlayerGroundedState.cs
+++ My project/Assets/PlayerGroundedState.cs
@@ -19,5 +19,11 @@ public class PlayerGroundedState : PlayerState
     public override void Update()
     {
         base.Update();
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            stateMachine.ChangeState(player.dashState);
+
+        if (Input.GetKeyDown(KeyCode.Space) && player.IsGroundDetected())
+            stateMachine.ChangeState(player.jumpState);
     }
 }
